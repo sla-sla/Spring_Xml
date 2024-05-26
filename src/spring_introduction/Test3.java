110b36fd3c60b956a013c3c404e5c23a33709953
Вячеслав Бекиров <60131716@leroymerlin.ru>
2024-05-26 04:07:41 +0300

    Add context person

--- src/spring_introduction/Test3.java
+++ src/spring_introduction/Test3.java
@@ -6,9 +6,10 @@ public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Pet pet = context.getBean("myPet", Pet.class);
+        //Pet pet = context.getBean("myPet", Pet.class);
         //Pet pet = new Dog();
-        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        context.close();
     }
 }
