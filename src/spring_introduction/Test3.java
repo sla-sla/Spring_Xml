18df5930edb6539bd656e28596a0c180661c5ac4
Вячеслав Бекиров <60131716@leroymerlin.ru>
2024-05-26 03:35:50 +0300

    Add person

---
+++ src/spring_introduction/Test3.java
@@ -0,0 +1,14 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myPet", Pet.class);
+        //Pet pet = new Dog();
+        Person person = new Person(pet);
+        person.callYourPet();
+    }
+}
