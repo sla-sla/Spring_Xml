d61a12b44db712b525a0fbe50fe84a25222e0340
Вячеслав Бекиров <60131716@leroymerlin.ru>
2024-05-26 04:20:02 +0300

    add setPet

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,9 +1,20 @@
 package spring_introduction;
 
+import io.micrometer.observation.transport.Propagator;
+
 public class Person {
     private Pet pet;
-    public Person(Pet pet) {
+    //public Person(Pet pet) {
+    //    System.out.println("Person bean is created");
+    //    this.pet = pet;
+   // }
+
+    public Person(){
         System.out.println("Person bean is created");
+    }
+    // pet-> setPet
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: Set pet");
         this.pet = pet;
     }
 
